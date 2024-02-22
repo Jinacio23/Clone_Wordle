a1b80c4ba579758db15822ed5fd9d44625649644
Jinacio23 <js.inacio746@gmail.com>
2024-02-22 20:04:27 -0300

    tentativas

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,12 @@
-const palpite = document.querySelector('.abilitado')
 const letra = document.querySelectorAll('.letra')
 let fileira = document.querySelector('.coluna.l1')
+let nodes = fileira.children
 
 const palavra = ['canoa','balao','arpeu']
 const alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+let linha = 1
 
-let nodes = palpite.children
-//console.log(nodes);
+console.log(nodes);
 
 //Resposta final
 function SelecionarPalavra() {
@@ -25,7 +25,7 @@ function focar(b) {
 
 tentativaAtual()
 
-checarResposta()
+checarPalavra()
 
 //Controle de teclas (De A - Z)
 letra.forEach(addEventListener('keypress', (e) => {
@@ -38,46 +38,69 @@ letra.forEach(addEventListener('keypress', (e) => {
 
 //Tentativa atual
 function tentativaAtual() {
-    for(let i = 0; i < nodes.length; i++){
-    nodes[i].removeAttribute('disabled')
-}
+    if(nodes[0].hasAttribute('disabled')){
+        for(let i = 0; i < nodes.length; i++){
+        nodes[i].removeAttribute('disabled')
+        }
+    } else {
+        for(let i = 0; i < nodes.length; i++){
+        nodes[i].setAttribute('disabled','true')
+        }
+    }
 }
 
 //Confirmar palpite
-function checarResposta() {
+function checarPalavra() {
     addEventListener('keypress', (e) => {
-        let tentativa = []
+        let count = 0
 
+        //caso falte completar a palavra
         if(e.keyCode === 13){
             for(let i = 0; i < nodes.length; i++){
                 if(nodes[i].value == ""){
                     alert('complete a palavra primeiro')
                     break
-                } 
+                } else {
+                    count++
+                    if(count == 5){
+                        checarResposta()
+                    }
+                }
             }  
-        }
-
-        for(let t = 0; t < nodes.length; t++){
-            tentativa.push(nodes[t].value)
-        }
-
-        if(tentativa.join("") == resposta.toUpperCase()){
-            alert('Parabéns, você acertou!')
-            //reiniciar jogo
-        } else {
-            //proxima tentativa
-        }
-           
+        }     
     })
 }
 
+function checarResposta() {
+    let tentativa = []
 
-//Proxima letra - tecla de espaço
-// function proximaLetra() {
-//     letra.forEach(addEventListener('keypress', (e) => { 
-//         let selecao = document.querySelector('selecionado')
-//         if(e.keyCode === 32) {
-          
-//         }
-//     }))
-// }
+    for(let t = 0; t < nodes.length; t++){
+        tentativa.push(nodes[t].value)
+    }
+
+    //caso resposta correta senão...
+    if(tentativa.join("") == resposta.toUpperCase()){
+        alert('Parabéns, você acertou!')
+        //reiniciar jogo
+
+    } else {//proxima tentativa
+        tentativaAtual()
+        //passa para próxima linha
+        linha++
+        if(linha < 7){//Máximo de tentativas permitidas 6, senão game over
+            //Atualiza e remove disabled da linha de tentativa atual
+            fileira = document.querySelector(`.coluna.l${linha}`)
+            nodes = fileira.children
+            tentativaAtual()
+        } else {
+            alert('Você perdeu')
+        }
+    }  
+}
+
+//Dicas/cores do jogo
+function adicionarCores() {
+//bgVerde - letra no local certo
+//bgAmarelo - letra existente, porém no lugar errado
+//bgVermelho - letra inexistente na palavra
+}
